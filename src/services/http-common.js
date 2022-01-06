7ad5fb71e2a34fabfe253b82e2646d22ca7b414d
biloocabba <ladoui.bilal@gmail.com>
2022-01-06 14:12:58 +0200

    add all

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "http://ec2-3-70-46-28.eu-central-1.compute.amazonaws.com:8080",
   headers: {
     "Content-type": "application/json",
     "Accept": "application/json"
